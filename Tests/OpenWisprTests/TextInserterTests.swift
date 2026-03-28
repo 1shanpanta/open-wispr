cae632f888587f6fedb3b968f97e8ebf0499a0fc
Ammon Taylor <48261561+human37@users.noreply.github.com>
2026-03-28 11:06:56 -0600

    Fix paste for non-QWERTY keyboard layouts (#41)
    * Fix paste for non-QWERTY keyboard layouts (Dvorak, etc.)
    
    * Remove inputMethod config option, use CGEvent with dynamic key code resolution for all layouts
    
    * v0.32.0

--- Tests/OpenWisprTests/TextInserterTests.swift
+++ Tests/OpenWisprTests/TextInserterTests.swift
@@ -3,22 +3,8 @@ import XCTest
 
 final class TextInserterTests: XCTestCase {
 
-    func testPasteKeyCodeIsHardcodedToVirtualV() {
-        XCTAssertEqual(TextInserter.pasteKeyCode, 9, "Paste key code must be 9 (virtual 'V' position) to work across all keyboard layouts")
-    }
-
-    func testDefaultPasteMethodIsCGEvent() {
-        let inserter = TextInserter(inputMethod: nil)
-        XCTAssertEqual(inserter.resolvedInputMethod, "cgevent")
-    }
-
-    func testAppleScriptPasteMethodIsRecognized() {
-        let inserter = TextInserter(inputMethod: "applescript")
-        XCTAssertEqual(inserter.resolvedInputMethod, "applescript")
-    }
-
-    func testUnknownPasteMethodDefaultsToCGEvent() {
-        let inserter = TextInserter(inputMethod: "garbage")
-        XCTAssertEqual(inserter.resolvedInputMethod, "cgevent")
+    func testPasteKeyCodeResolvesForCurrentLayout() {
+        let inserter = TextInserter()
+        XCTAssertTrue(inserter.pasteKeyCode < 128, "Paste key code should be a valid virtual key code")
     }
 }
